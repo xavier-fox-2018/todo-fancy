fa598f47cc2c76d8d8d8435745026f5ee63c1b68
Nsamudera <nsamudera12@gmail.com>
2018-11-04 22:35:29 +0700

    Changes for heroku deploy

--- server/app.js
+++ server/app.js
@@ -1,16 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const mongoose = require('mongoose')
 const cors = require('cors')
 //routes
 const toDoRoute = require('./routes/index.js')
 
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
-
 
 //connect mongoose
 mongoose.connect('mongodb://localhost:27017/todo', {useNewUrlParser:true});
